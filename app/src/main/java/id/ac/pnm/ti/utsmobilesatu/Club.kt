d5089a6fb8d3b6d9f9a82696b20a0114a85f3b58
DimssOrigin <dimssfeibufight@gmail.com>
2024-03-24 21:05:15 +0700

    uts pemrograman mobile -dimas

--- app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Club.kt
+++ app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Club.kt
@@ -1,16 +1,27 @@
-package ...
+package id.ac.pnm.ti.utsmobilesatu
 
 // data class Club
 data class Club(
     val name: String,
-    val epl: ...
-    dst...
+    val lpi: Int,
+    val fa: Int,
+    val efl: Int,
+    val lce: Int,
+    val lcu: Int
 ) {
     val totalTrophy: Int
-        get() = ...
+        get() = lpi+fa+efl+lce+lcu
 }
+
+//class yang berguna untuk menyimpan data
 
 // extension function recap
 fun Club.recap(): String {
-    return ...
+    return "$name berhasil meraih $lpi trofi Liga Premier Inggris," +
+            "$fa trofi FA" +
+            "$efl trofi EFL" +
+            "$lce trofi Liga Champions" +
+            "$lcu trofi Liga Eropa EUFA"
 }
+
+//function yang digunakan untuk mencetak recap hasil liga
--- app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Main.kt
+++ app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Main.kt
@@ -1,49 +1,66 @@
-package ...
-
+package id.ac.pnm.ti.utsmobilesatu
 fun main() {
     // Inisialisasi mutableList dan tambah data List
-    val clubs = ...
-    clubs.add(...)
-    dst...
+    val clubs = mutableListOf(Club("Liverpool",19,8,9,6,3))
+            clubs.add(Club("Manchester United", 20 ,12,6,3,1))
+            clubs.add(Club("Chelsea", 6 ,6,5,2,2))
+            clubs.add(Club("Manchester City", 8 ,8,8,0,0))
+            clubs.add(Club("Arsenal", 13 ,14,2,0,0))
+
+    //variable ini menyimpan list beberapa klub beserta trophy yang dimenangkan di tiap liga
 
     // Passing a named function ::sortByNumberTrophyTotal
-    val sortedClubs = ...
+    val sortedClubs = filterAndSort(clubs, ::sortByNumberTrophyTotal)
+    sekat()
     // Hasil sorting total trofi
     println("Urutan klub berdasarkan jumlah total trofi: ")
-    for (club in ...) {
-        ...
+    for (club in sortedClubs) {
+        println("${club.name} telah mendapatkan total trophy sebanyak ${club.totalTrophy}" )
     }
+
+    //variable ini digunakan untuk menampung sortedclubs function untuk melempar sortbynumbertophytotal
 
     // Passing a named function ::filterByEuropeanTrophy
-    val filteredClubs = ...
+    val filteredClubs = filterAndSort(clubs, ::filterByEuropeanTrophy)
+    sekat()
     // Hasil sorting total trofi
     println("Klub yang belum pernah memenangkan UCL dan UEL:")
-    for (club in ...) {
-        ...
+    for (club in filteredClubs) {
+        println("${club.name}")
     }
-
+    //varibable filterclub digunakan untuk menampung high order function untuk melempar function filterbyeuropean function
+sekat()
     // Panggil extension function Club recap
-    println(...)
+    println(clubs[0].recap())
 }
 
 // High order function filter and sort
 fun filterAndSort(clubs: List<Club>, options: (List<Club>) -> List<Club>): List<Club> {
-    return ...
+    return options(clubs)
 }
+
+//function ini digunakan untuk melempar function filterandsort
 
 // Fungsi sorting
 fun sortByNumberTrophyTotal(clubs: List<Club>): List<Club> {
     val sortedClubs = clubs.sortedByDescending {
-        ...
+        it.totalTrophy
     }
     return sortedClubs
 
 }
+//function ini digunakan sebagai high order function yang berguna untuk melempar function lain
 
 // Fungsi filter
 fun filterByEuropeanTrophy(clubs: List<Club>): List<Club> {
     val filteredClubs = clubs.filter {
-        ...
+        it.lcu == 0 && it.lce == 0
     }
     return filteredClubs
 }
+//jadi properti lcu sama dengan 0 dan lcu sama dengan 0 yang digunakann untuk memfilter trophy yang 0
+
+fun sekat(){
+    println("+==============================================================================+")
+}
+//digunakan untuk memberikan function sekat untuk menambahkan sekat pada baris
